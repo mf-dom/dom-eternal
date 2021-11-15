711c6b64814bf296f74545238119374123052091
Brian Wang <brianwang2018@gmail.com>
2021-11-15 00:31:38 -0600

    popup landing page frontend complete

---
+++ popup-src/popup.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const record = document.getElementById('recordbtn');
+    const stop = document.getElementById('stopbtn');
+    const settings = document.getElementById('settings');
+    
+    record.addEventListener("click", function() {
+
+    });
+    
+    stop.addEventListener("click", function() {
+
+    });
+
+    settings.addEventListener("click", function() {
+        window.location.href = "settings.html";
+    });
+});
