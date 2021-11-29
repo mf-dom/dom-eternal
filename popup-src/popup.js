77d61b53e2259e47935139e2bcb4cbf32b5024c5
Brian <brianwang2018@gmail.com>
2021-11-29 00:41:49 -0600

    functional search in results, overall more user feedback in popup ui, added template info page

--- popup-src/popup.js
+++ popup-src/popup.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const record = document.getElementById('recordbtn');
     const stop = document.getElementById('stopbtn');
     const settings = document.getElementById('settings');
+    const info = document.getElementById("info");
     
     record.addEventListener("click", function() {
         document.getElementById("recordbtn").style.cursor = "not-allowed";
@@ -9,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     
     stop.addEventListener("click", function() {
+        if (!confirm("Stop recording and see results?")) {
+            return;
+        }
         window.location.href = "results.html";
         document.getElementById("recordbtn").disabled = "false";
         document.getElementById("recordbtn").style.cursor = "unset";
@@ -16,5 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     settings.addEventListener("click", function() {
         chrome.runtime.openOptionsPage();
+    });
+
+    info.addEventListener("click", function() {
+        window.location.href = "info.html";
     });
 });
--- popup-src/results.js
+++ popup-src/results.js
@@ -1,22 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const search = document.getElementById("search-button");
+document.addEventListener("DOMContentLoaded", function () {
     const filter = document.getElementById("filter-button");
     const settings = document.getElementById("settings-button");
-    const fuzz = document.getElementById("fuzz-button")
+    const fuzz = document.getElementById("fuzz-button");
+    const search_input = document.getElementById("search-input");
+    const select_all = document.getElementById("select-all");
 
-    search.addEventListener("click", function() {
+    search_input.addEventListener("keyup", function () {
+        var input, filter, ul, li, label, i, txtValue;
+        filter = search_input.value.toUpperCase();
+        ul = document.getElementById("results-ul");
+        li = ul.getElementsByTagName('li');
+
+        for (i = 0; i < li.length; i++) {
+            label = li[i].getElementsByTagName("label")[0];
+            txtValue = label.textContent || label.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    });
+
+    select_all.addEventListener("click", function () {
+        checkboxes = document.getElementsByName("result_cb");
+        for (var i = 0, n = checkboxes.length; i < n; i++) {
+            checkboxes[i].checked = select_all.checked;
+        }
+    });
+
+    filter.addEventListener("click", function () {
         
     });
 
-    filter.addEventListener("click", function() {
-        
-    });
-
-    fuzz.addEventListener("click", function() {
+    fuzz.addEventListener("click", function () {
         window.location.href = "report.html";
     });
 
-    settings.addEventListener("click", function() {
+    settings.addEventListener("click", function () {
         chrome.runtime.openOptionsPage();
     });
 });
